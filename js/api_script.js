a39d2fba9bbaa10608afc5bc58039c377455b690
cwryd <cryder9898@gmail.com>
2017-01-13 14:53:03 -0500

    added correct ajax requests and created list of streamers

--- js/api_script.js
+++ js/api_script.js
@@ -1,37 +1,75 @@
-var streamers = ['ESL_SC2','OgamingSC2','cretetion','freecodecamp','storbeck','habathcx','RobotCaleb','noobs2ninjas','brunofin','comster404'];
+var channels = ['ESL_SC2','OgamingSC2','cretetion','freecodecamp','storbeck','habathcx','RobotCaleb','noobs2ninjas','brunofin','comster404'];
 var clientID = 'rpkd7d5ra8zsd5jgbupb4nuywh7ivc';
-var baseURL = 'https://www.twitch.tv/';
+var baseUrl = 'https://api.twitch.tv/kraken';
 
-$(document).ready(function() {
-	getLiveStreamers();
-	setList(data);	
-});
+function getChannelUrl (data) {
+	return data._links.channel;
+}
 
-function getLiveStreamers() {
+function addListItem(isOnline, data) {
+	var li = '';
+	li += '<a href="' + data.url + '" class="list-group-item" target="_blank">';
+	li += '<img class="logo" src="' + data.logo + '"' + 'alt="logo">';
+	li += '<h4 class="list-group-item-heading">' + data.display_name + '</h4>';
+	li += '<p class="list-group-item-text">' + data.status + '</p>';
+	if (isOnline) {
+		li += 'ONLINE';
+	} else {
+		li += 'OFFLINE';
+	}
+	li += '</a>'
+	$('#streamer-list').append(li);
+}
+
+function setChannelInfo(isOnline, channel) {
+	// retrieving channel information
 	$.ajax({
-		url: 'https://api.twitch.tv/kraken/streams?channel=' + streamers.toString(),
+		url: baseUrl + '/channels/' + channel,
+		dataType: 'json',
 		headers: {
 		'Client-ID': clientID
 		},
 		success: function(data) {
 			console.log(data);
-	   		setList(data);
- 		}
+			addListItem(isOnline, data);
+
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			//console.log(jqXHR);
+			var msg = '';
+			if (jqXHR.status === 404) {
+				msg = 'Channel "' + channel + '"" does not exist';
+				$('#exist').append('<li>' + msg + '</li>');
+			}
+		}
 	});
 }
 
-function setList(data) {
-	var list = '';
-	for (var i = 0; i < streamers.length; i++) {
-		var status = 'OFFLINE';
-		for (var j = 0; j < data.streams.length; j++) {
-			if (data.streams[j].channel.display_name === streamers[i]) {
-				status = 'LIVE';
-			}
-		}
-		list += '<li><a href="' + baseURL + streamers[i] + '">' + streamers[i] + '</a> ' + status + '</li>';
-	}
-	$('#streamer-list').append(list);
+function updateList() {
+	// retrieving online streamers
+	channels.forEach(function(channel) {
+		$.ajax({
+			url: baseUrl + '/streams/' + channel,
+			dataType: 'json',
+			headers: {
+			'Client-ID': clientID
+			},
+			success: function(data) {
+				var channelUrl = getChannelUrl(data);
+				//console.log(data);
+				if (data.stream) {
+					// ONLINE
+		   			setChannelInfo(true, channel);
+				} else {
+					// OFFLINE
+					setChannelInfo(false, channel);
+				}
+ 			},
+		});
+	});
+	
 }
 
-
+$(document).ready(function() {
+	updateList();
+});
